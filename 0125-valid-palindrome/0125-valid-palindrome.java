60d639661a4e3980f3ec733f9c151ad5587cc05b
Phalgunigithub <97692467+Phalgunigithub@users.noreply.github.com>
2023-12-09 17:54:59 +0530

    Time: 21 ms (30.58%), Space: 44.2 MB (21.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-       String cleanString = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        // Reverse the cleaned string
-        String reversed = new StringBuilder(cleanString).reverse().toString();
-
-        // Check if the original cleaned string is equal to its reversed version
-        if (cleanString.equals(reversed)) {
+            String alphanum=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        
+    StringBuilder str= new StringBuilder(alphanum);
+            
+        if(alphanum.equals(str.reverse().toString())){
             return true;
-        } else {
-            return false;  // or return cleanString; or any other meaningful value
         }
+        
+        else 
+            return false;
         
     }
 }
